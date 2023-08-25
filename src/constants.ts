c0f9b920e6ce641f8eb3eebab17d73574fc5513c
Fabio Spampinato <spampinabio@gmail.com>
2023-08-25 12:01:46 +0100

    Attempting to detect piping, and turning off colors in that case

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@
 const ENV = globalThis.process?.env || {};
 const ARGV = globalThis.process?.argv || [];
 
-const IS_ENABLED = !( 'NO_COLOR' in ENV ) && ( ENV.TERM !== 'dumb' ) && !ARGV.includes ( '--no-color' ) && !ARGV.includes ( '--no-colors' );
+const IS_ENABLED = !( 'NO_COLOR' in ENV ) && ( ENV.TERM !== 'dumb' ) && !ARGV.includes ( '--no-color' ) && !ARGV.includes ( '--no-colors' ) && ( globalThis.process?.stdout?.isTTY !== false );
 
 /* EXPORT */
 
