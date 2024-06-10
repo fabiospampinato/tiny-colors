30513cb14b4f226361cc098444cb5726ad370d67
Fabio Spampinato <spampinabio@gmail.com>
2024-06-10 21:57:51 +0100

    Looking also for the value of the "COLOR" env variable, useful to force-enable colors

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@
 const ENV = globalThis.process?.env || {};
 const ARGV = globalThis.process?.argv || [];
 
-const ENABLED = !( 'NO_COLOR' in ENV ) && ( ENV.TERM !== 'dumb' ) && !ARGV.includes ( '--no-color' ) && !ARGV.includes ( '--no-colors' ) && ( !globalThis.process?.stdout || globalThis.process?.stdout?.isTTY === true );
+const ENABLED = !( 'NO_COLOR' in ENV ) && ( ENV.COLOR !== '0' ) && ( ENV.TERM !== 'dumb' ) && !ARGV.includes ( '--no-color' ) && !ARGV.includes ( '--no-colors' ) && ( ENV.COLOR === '1' || !globalThis.process?.stdout || globalThis.process?.stdout?.isTTY === true );
 
 /* EXPORT */
 
